6acf566e2903caa10cd1d6a9ecc53ec91fc57387
Renaldo Viola <renaldo@Rocketlab.com.br>
2020-07-22 23:19:19 -0300

    implementa totalItensInCart e cartTotal.

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -15,24 +15,31 @@ import formatValue from '../../utils/formatValue';
 
 import { useCart } from '../../hooks/cart';
 
-// Calculo do total
-// Navegação no clique do TouchableHighlight
-
 const FloatingCart: React.FC = () => {
   const { products } = useCart();
 
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    let valueItemCart = 0;
+    let sumValueCart = 0;
 
-    return formatValue(0);
+    products.forEach(product => {
+      valueItemCart = product.quantity * product.price;
+      sumValueCart += valueItemCart;
+    });
+
+    return formatValue(sumValueCart);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    let countItemCart = 0;
 
-    return 0;
+    products.forEach(product => {
+      countItemCart += product.quantity;
+    });
+
+    return countItemCart;
   }, [products]);
 
   return (
